dbd9b3fbae90f1dc2299cf98e6ad01c3c0a7a590
Chuks Victor <vchuks70@gmail.com>
2021-05-02 08:43:24 +0000

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+             return View();
+            
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = " + id);
+
+        }
+    }
+}
